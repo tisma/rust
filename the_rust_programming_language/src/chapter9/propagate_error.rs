9a339a558ce263443dfa307fd77b254d8ae0f3a5
Miroslav Tisma <tisma.etf@gmail.com>
2019-07-03 22:57:24 +0200

    Add example of propagation of error to caller function.

---
+++ the_rust_programming_language/src/chapter9/propagate_error.rs
@@ -0,0 +1,31 @@
+use std::fs::File;
+use std::io::Read;
+use std::io;
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+
+fn read_username_from_file_propagate() -> Result<String, io::Error> {
+    let mut s = String::new();
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn main() {
+    // let _f = File::open("hello.txt").unwrap();
+
+    let _ff = File::open("foo.txt").expect("Failed to open foo.txt");
+}
