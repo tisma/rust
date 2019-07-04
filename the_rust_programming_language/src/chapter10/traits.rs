66b5d7b12857fc37f8f63fe00a4693001f7d6d5a
Miroslav Tisma <tisma.etf@gmail.com>
2019-07-04 22:59:44 +0200

    Change Summary trait to have default function.

--- the_rust_programming_language/src/chapter10/traits.rs
+++ the_rust_programming_language/src/chapter10/traits.rs
@@ -1,5 +1,9 @@
 pub trait Summary {
-    fn summarize(&self) -> String;
+    fn sumarize_author(&self) -> String;
+
+    fn summarize(&self) -> String {
+        format!("(Read more from {}...)", self.sumarize_author())
+    }
 }
 
 pub struct NewsArticle {
@@ -10,8 +14,8 @@ pub struct NewsArticle {
 }
 
 impl Summary for NewsArticle {
-    fn summarize(&self) -> String {
-        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    fn sumarize_author(&self) -> String {
+        format!("@{}", self.author)
     }
 }
 
@@ -23,18 +27,26 @@ pub struct Tweet {
 }
 
 impl Summary for Tweet {
-    fn summarize(&self) -> String {
-        format!("{}: {}", self.username, self.content)
+    fn sumarize_author(&self) -> String {
+        format!("@{}", self.username)
     }
 }
 
 fn main() {
     let tweet = Tweet {
-        username: String::from("horese_ebooks"),
+        username: String::from("horse_ebooks"),
         content: String::from("of course, as you probably already know, people"),
         reply: false,
         retweet: false,
     };
 
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from("The Pittsburgh Penguins once again are the best hockey team in the NHL."),
+    };
+
     println!("1 new tweet: {}", tweet.summarize());
+    println!("New article available! {}", article.summarize());
 }
