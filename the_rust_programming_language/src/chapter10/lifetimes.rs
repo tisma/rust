f5b678262023482fd1c88d00d0787415059d6505
Miroslav Tisma <tisma.etf@gmail.com>
2019-07-07 22:05:51 +0200

    Add references lifetimes example.

---
+++ the_rust_programming_language/src/chapter10/lifetimes.rs
@@ -0,0 +1,59 @@
+use std::fmt::Display;
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+#[derive(Debug)]
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn main() {
+    {
+        let r;
+        let x = 5;
+        r = &x;
+        println!("r: {}", r);
+    }
+
+    {
+        let string1 = String::from("abcd");
+        let string2 = "xyz";
+
+        let result = longest(string1.as_str(), string2);
+        println!("The longest string is {}", result);
+    }
+
+    let string1 = String::from("long string is long");
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result);
+    }
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split(".").next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+    println!("{:#?}", i);
+
+    println!("{}", longest_with_an_announcement("test", "example string", "announcement string"));
+}
