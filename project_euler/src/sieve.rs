65ecff2bbabbd92d61be8f72bf5fdab901a78fa7
Miroslav Tisma <tisma.etf@gmail.com>
2019-08-22 22:56:59 +0200

    Project Euler problems solved with rust.

---
+++ project_euler/src/sieve.rs
@@ -0,0 +1,32 @@
+static MAX: usize = 1_000_000;
+
+fn sieve(n: usize) -> usize {
+    let data: Vec<usize> = (0..MAX).collect();
+    let mut isprime: Vec<bool> = vec![true; data.len()];
+    isprime[0] = false;
+    for (i, d) in data.iter().enumerate() {
+        let offset = *d;
+        if !isprime[i] || offset == 1 {
+            continue;
+        }
+        for j in ((i + offset)..isprime.len()).step_by(offset) {
+            isprime[j as usize] = false
+        }
+    }
+
+    let primes = isprime
+        .iter()
+        .enumerate()
+        .fold(vec![], |mut acc, (index, value)| {
+            if *value {
+                acc.push(index)
+            }
+            acc
+        });
+
+    primes[n]
+}
+
+fn main() {
+    println!("{}", sieve(2300));
+}
